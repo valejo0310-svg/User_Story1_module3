317b16cfb3013fae7bb79e005c63829693d7326c
Valery Avila <valejo0310@gmail.com>
2026-05-14 19:05:58 -0500

    add comments for the interactive system
    Refactor variable declarations and improve prompts for user input.

--- interactive_system.js
+++ interactive_system.js
@@ -1,19 +1,21 @@
-const btn = document.querySelector(".btn");
-let name, age;
+const btn = document.querySelector(".btn"); //Const used to call the class button from the html
+let name, age; // variables that will be redefined later on the code
 
-btn.addEventListener ("click", () => {
-   name = prompt ("Please enter your name: ");
-   while (name.trim()=== ""){
+btn.addEventListener ("click", () => { //arrow function to add an event to the button 
+   name = prompt ("Please enter your name: "); //Redefinition of the name variable to be stored
+   while (name.trim()=== ""){ //error control using a loop that while stay until the answer is right
     alert ("please enter a name");
     name = prompt ("enter your name");
    };
    
-   age = prompt ("Please enter you age");
-   while (isNaN(age) || age.trim()===""){
+   age = prompt ("Please enter you age");//Redefinition of the age variable to be stored
+   while (isNaN(age) || age.trim()===""){ //error control using a loop that while stay until the answer is right
     alert("Please enter the right value");
     age = prompt ("Please enter your age");
    }
+   //Conditionals using ternary operators to prevent from using so many code lines.
    let confirm = (age >= 18) ? alert ("You're an adult, you can keep going") : alert ("You're underage, please wait a little.")
+   
 
 });
 
